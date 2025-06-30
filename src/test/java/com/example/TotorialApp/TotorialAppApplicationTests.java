cb8df94d72884f6a1c5d2e9d80fef69e5628f463
Ahmed Amin <ahmedabdelrasoul7@gmail.com>
2025-06-30 11:53:06 +0300

    first commit

---
+++ src/test/java/com/example/TotorialApp/TotorialAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TotorialApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotorialAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
